845d48611f5ce3b6fc000fb616bec7ffa88a532d
ProstoNikich <ojmlja@gmail.com>
2019-11-18 09:13:16 +0500

    PECW-65 Для врагов c дистанционной атакой был разработан модуль E_RangeAttack и Bullet

---
+++ Assets/Scripts/Gameplay/Enemy/E_RangeAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_RangeAttack : AttackEvent
+{
+    [SerializeField] GameObject bullet;
+    [SerializeField] float cooldownAttack = 2.5f;
+    [SerializeField] bool attackWithAgro = false;
+    float timer = 0f;
+
+    E_AgroZone agroZone;
+
+     void Start()
+    {
+        agroZone = transform.parent.GetComponentInChildren<E_AgroZone>();
+    }
+
+    void Update()
+    {
+        if (timer > 0) timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            if (attackWithAgro && agroZone != null && agroZone.IsAgred) Attack();
+            else if (attackWithAgro && (agroZone == null || !agroZone.enabled))
+                Debug.LogError("У объекта " + transform.parent.name + " отсутствует модуль AgroZone");
+            else if (!attackWithAgro) Attack();
+        }
+    }
+
+    public override void Attack()
+    {
+        Instantiate(bullet, transform.position, transform.rotation);
+        timer = cooldownAttack;
+    }
+}
---
+++ Assets/Scripts/Gameplay/Other/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  Снаряд летящий в перед по оси Z, наносит урон при столкновении и самоуничтожается
+/// </summary>
+[System.Serializable]
+public class Bullet : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    [SerializeField] float m_Speed = 1.0f;
+    [SerializeField] float deadDelay = 0.1f;
+    [SerializeField] bool infinityLive = true;
+    [SerializeField] float timeLive = 300f;
+    DamageZone damageZone;
+
+    void Start()
+    {
+        damageZone = GetComponent<DamageZone>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        m_Rigidbody.velocity = transform.forward * m_Speed; //а не в FixedUpdate() ?
+    }
+    private void Update()
+    {
+        if(timeLive<=0 && !infinityLive) StartCoroutine(Dead());
+        if (!infinityLive) timeLive -= Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (other.CompareTag(damageZone.targetTag.ToString()))
+        //    StartCoroutine(Dead());
+        if (other.CompareTag(damageZone.targetTag.ToString()) || other.CompareTag("Untagged"))
+            StartCoroutine(Dead());
+    }
+
+    IEnumerator Dead()
+    {
+        //Запускаем анимацию
+        yield return new WaitForSeconds(deadDelay);
+        Destroy(this.gameObject);
+    }
+
+}
